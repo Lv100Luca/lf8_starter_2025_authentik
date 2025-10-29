2324a047462ba51748254a31a95ca90d628ec92a
Köpernick, Leon <Leon.Koepernick@adesso.de>
2025-10-29 12:29:38 +0100

    employee Structure initial commit

---
+++ src/main/java/de/szut/lf8_starter/employee/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package de.szut.lf8_starter.employee.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+}
