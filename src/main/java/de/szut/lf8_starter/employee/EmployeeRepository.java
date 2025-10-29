3a16903c7fb3092aa0c93acc1b523b20bd59494b
Köpernick, Leon <Leon.Koepernick@adesso.de>
2025-10-29 12:30:09 +0100

    small fix

--- src/main/java/de/szut/lf8_starter/employee/EmployeeRepository.java
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeRepository.java
@@ -1,9 +1,8 @@
 package de.szut.lf8_starter.employee;
 
-import de.szut.lf8_starter.projects.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<ProjectEntity, Long> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 }
