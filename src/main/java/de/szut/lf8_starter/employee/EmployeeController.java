323efa1c0a340b94617dc0df91d9ce8f2fa6e076
Köpernick, Leon <Leon.Koepernick@adesso.de>
2025-11-05 09:16:26 +0100

    connected employee and project in db

--- src/main/java/de/szut/lf8_starter/employee/EmployeeController.java
+++ src/main/java/de/szut/lf8_starter/employee/EmployeeController.java
@@ -1,47 +1,47 @@
-package de.szut.lf8_starter.employee;
-
-import de.szut.lf8_starter.employee.service.EmployeeService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/employee")
-@RequiredArgsConstructor
-public class EmployeeController {
-
-    private final EmployeeService employeeService;
-
-    @GetMapping("/projects/{projectId}/employee")
-    public ResponseEntity<EmployeeEntity> getAllEmployeesFromProject(@PathVariable Long projectId) {
-
-        //TODO logic
-
-        return ResponseEntity.ok(new EmployeeEntity(projectId));
-    }
-
-    @DeleteMapping("/projects/{projectId}/employee/{employeeId}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable Long projectId, @PathVariable Long employeeId) {
-
-        //TODO logic
-
-        String response = "Employee with id " + employeeId + " was deleted, from project " + projectId + "!";
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
-    }
+//package de.szut.lf8_starter.employee;
 //
-//    @PostMapping("projects/{projectId}/employee/{employeeId}")
-//    public ResponseEntity<GetProjectDto> addAnExistingPlayerToAProject(@PathVariable Long projectId, @PathVariable Long employeeId) {
+//import de.szut.lf8_starter.employee.service.EmployeeService;
+//import jakarta.validation.Valid;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
 //
-//        ProjectEntity project = projectService.getProjectById(projectId);
-//        EmployeeEntity employee = employeeService.getEmployeeById(employeeId);
+//@RestController
+//@RequestMapping("/employee")
+//@RequiredArgsConstructor
+//public class EmployeeController {
 //
-//        project.getEmployeeList.add(employee);
-//        GetProjectDto projectDto = projectMappingService.convert(project);
+//    private final EmployeeService employeeService;
 //
-//        return ResponseEntity.status(HttpStatus.CREATED).body(projectDto);
+//    @GetMapping("/projects/{projectId}/employee")
+//    public ResponseEntity<EmployeeEntity> getAllEmployeesFromProject(@PathVariable Long projectId) {
+//
+//        //TODO logic
+//
+//        return ResponseEntity.ok(new EmployeeEntity(projectId));
 //    }
-}
+//
+//    @DeleteMapping("/projects/{projectId}/employee/{employeeId}")
+//    public ResponseEntity<String> deleteEmployee(@PathVariable Long projectId, @PathVariable Long employeeId) {
+//
+//        //TODO logic
+//
+//        String response = "Employee with id " + employeeId + " was deleted, from project " + projectId + "!";
+//        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(response);
+//    }
+////
+////    @PostMapping("projects/{projectId}/employee/{employeeId}")
+////    public ResponseEntity<GetProjectDto> addAnExistingPlayerToAProject(@PathVariable Long projectId, @PathVariable Long employeeId) {
+////
+////        ProjectEntity project = projectService.getProjectById(projectId);
+////        EmployeeEntity employee = employeeService.getEmployeeById(employeeId);
+////
+////        project.getEmployeeList.add(employee);
+////        GetProjectDto projectDto = projectMappingService.convert(project);
+////
+////        return ResponseEntity.status(HttpStatus.CREATED).body(projectDto);
+////    }
+//}
